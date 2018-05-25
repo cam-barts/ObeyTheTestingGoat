e667a83ceff31d885636622231f41e6fca0b27ea
Cam Barts <CameronD.Barts@gmail.com>
2018-05-25 14:25:32 -0700

    Passed 404 test

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from lists import views
+from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', views.home_page, name='home'),
 ]
