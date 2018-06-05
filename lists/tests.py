d4e15dd93e920e3b5147dc160491bd747ee3e4b3
Cam Barts <CameronD.Barts@gmail.com>
2018-06-05 11:21:11 -0700

    Redirects to correct templates, url for creating new lists

--- lists/tests.py
+++ lists/tests.py
@@ -10,23 +10,6 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_save_a_POST_request(self):
-        text = 'A new list item'
-        self.client.post('/', data={'item_text': text})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, text)
-
-    def test_redirects_after_POST(self):
-        text = 'A new list item'
-        response = self.client.post('/', data={'item_text': text})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
 
 class ItemModelTest(TestCase):
 
@@ -55,8 +38,21 @@ class LiveViewTest(TestCase):
     def test_displays_all_items(self):
         Item.objects.create(text='item 1')
         Item.objects.create(text='item 2')
-
         response = self.client.get('/lists/the-only-list-in-the-world/')
-
         self.assertContains(response, 'item 1')
         self.assertContains(response, 'item 2')
+
+
+class NewLiveTest(TestCase):
+
+    def test_can_save_a_POST_request(self):
+        text = 'A new list item'
+        self.client.post('/lists/new', data={'item_text': text})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, text)
+
+    def test_redirects_after_POST(self):
+        text = 'A new list item'
+        response = self.client.post('/lists/new', data={'item_text': text})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
