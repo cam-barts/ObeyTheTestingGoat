3d7d897fc1317272c90a9b88cbe0462147a90b12
Cam Barts <CameronD.Barts@gmail.com>
2018-06-05 12:53:33 -0700

    Added migration to db to parse lists

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 # from django.http import HttpRequest
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -11,23 +11,34 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items =Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
+
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 class LiveViewTest(TestCase):
 
@@ -36,8 +47,9 @@ class LiveViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='item 1')
-        Item.objects.create(text='item 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='item 1', list=list_)
+        Item.objects.create(text='item 2', list=list_)
         response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertContains(response, 'item 1')
         self.assertContains(response, 'item 2')
