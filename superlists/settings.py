12048a6bded5aa65950c9859c60d9161e0687622
CamDevServer <CameronD.Barts@gmail.com>
2018-06-11 01:32:20 +0000

    Fixed static root

--- superlists/settings.py
+++ superlists/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    #'django.contrib.admin',
+   # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -118,5 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR,'../static'))
+STATIC_URL = '../static/'
+STATIC_ROOT =os.path.abspath(os.path.join(BASE_DIR, '../static'))
