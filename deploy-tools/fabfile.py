5aa8def26276b356324b530bb0fdd0802a7b1710
Cam Barts <CameronD.Barts@gmail.com>
2018-06-18 14:59:57 -0700

    Added command to fabfile

--- deploy-tools/fabfile.py
+++ deploy-tools/fabfile.py
@@ -15,6 +15,15 @@ def deploy():
     _update_static_files(source_folder)
     _update_database(source_folder)
 
+def latest():
+    site_folder = f'/home/{env.user}/sites/{env.host}'
+    source_folder = site_folder + '/source'
+    _get_latest_source(source_folder)
+    _update_settings(source_folder, env.host)
+    _update_virtualenv(source_folder)
+    _update_static_files(source_folder)
+    _update_database(source_folder)
+
 def _create_directory_structure_if_necessary(site_folder):
     for subfolder in ('database', 'static', 'virtualenv', 'source'):
         run(f'mkdir -p {site_folder}/{subfolder}')
