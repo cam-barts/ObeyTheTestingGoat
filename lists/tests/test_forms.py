9aa4d52ae6dd23f6b6ec92ed1e7324181ba0c5ac
Cameron <CameronD.Barts@gmail.com>
2018-07-12 07:52:03 -0700

    New form for list items

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
 
-from lists.forms import ItemForm
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 
 
 class ItemFormTest(TestCase):
@@ -9,3 +9,8 @@ class ItemFormTest(TestCase):
         form = ItemForm()
         self.assertIn('placeholder="Enter a to-do item"', form.as_p())
         self.assertIn('class="form-control input-lg"', form.as_p())
+
+    def test_form_validation_for_blank_item(self):
+        form = ItemForm(data={"text": ""})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors["text"], [EMPTY_ITEM_ERROR])
