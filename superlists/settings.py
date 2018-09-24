586dde75dd82e29107be39d329067c6134a9729c
Cameron <CameronD.Barts@gmail.com>
2018-09-23 18:50:08 -0700

    Made email the primary key, #320

--- superlists/settings.py
+++ superlists/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "lists",
+    "accounts",
 ]
+
+AUTH_USER_MODEL = "accounts.User"
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -114,3 +118,12 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, "../static"))
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {"console": {"level": "DEBUG", "class": "logging.StreamHandler"}},
+    "loggers": {"django": {"handlers": ["console"]}},
+    "root": {"level": "INFO"},
+}
